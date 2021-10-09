e5bd9e40988e0ff913e0d7660da4e6426cfd8eb3
Armando <armandoramossanchez@gmail.com>
2021-10-09 22:09:05 +0200

    configuro estilo de toda la pagina

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 //ENTORNO
+let milisegundos = 0;
+
 let segundos = 0;
 
 let minutos = 0;
 
 let horas = 0;
+
+let miliSegundosPagina = document.getElementById("milisegundos");
 
 let segundosPagina = document.getElementById("segundos");
 
@@ -19,9 +23,14 @@ class crono{
 
     static start(){
         empezarCrono = setInterval(() => {
-            segundos++
-            (segundos < 10) ? segundosPagina.innerHTML = "0" + segundos : segundosPagina.innerHTML = segundos;
-            console.log(segundos);
+            (milisegundos < 10) ? miliSegundosPagina.innerHTML = "0" + milisegundos : miliSegundosPagina.innerHTML = milisegundos;
+            milisegundos++;
+            if (milisegundos == 100) {
+                miliSegundosPagina.innerHTML = "0" + 0;
+                milisegundos = 0;
+                segundos++;
+                (segundos < 10) ? segundosPagina.innerHTML = "0" + segundos : segundosPagina.innerHTML = segundos;
+            }
             if (segundos == 60) {
                 segundosPagina.innerHTML = "0" + 0;
                 segundos = 0;
@@ -32,13 +41,25 @@ class crono{
                 minutosPagina.innerHTML = "0" + 0;
                 minutos = 0;
                 horas++;
-                horasPagina.innerHTML = horas;
+                horasPagina.innerHTML = "0" + horas;
             }
-        }, 1000);
+        }, 10);
     }
 
     static pause(){
         clearInterval(empezarCrono);
+    }
+
+    static reset(){
+        clearInterval(empezarCrono);
+        milisegundos = 0;
+        segundos = 0;
+        minutos = 0;
+        horas = 0;
+        miliSegundosPagina.innerHTML = "0" + 0;
+        segundosPagina.innerHTML = "0" + 0;
+        minutosPagina.innerHTML = "0" + 0;
+        horasPagina.innerHTML = "0" + 0;
     }
 
 };
